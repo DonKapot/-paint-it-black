c31e1fbf51510fe6dbad158ddf70ddd9de25ab08
DonKapot <alexdanin@mail.ru>
2019-05-12 21:19:26 +0300

    Set actions: set up resetColor handler

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,17 +1,18 @@
 'use strict';
 
 let changeColor = document.getElementById('changeColor');
+let resetColor = document.getElementById('resetColor');
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = function(element) {
-
+changeColor.addEventListener('click', (e) => {
+  console.log("aaaaa");
   document.body.style.backgroundColor = "red";
 
-  let color = element.target.value;
+  let color = e.target.value;
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
@@ -19,4 +20,9 @@ changeColor.onclick = function(element) {
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
   });
 
-};
+});
+
+resetColor.addEventListener('click', (e) => {
+  console.log("ooooo");
+});
+
