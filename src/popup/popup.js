b787c0898f06931e368c30c5f54e4102470bb01d
DonKapot <alexdanin@mail.ru>
2019-05-26 19:04:27 +0300

    add content script

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,28 +1,34 @@
-'use strict';
+ 'use strict';
+ let changeColor = document.getElementById('changeColor');
+ let resetColor = document.getElementById('resetColor');
+ let buttons = document.querySelectorAll('button');
 
-let changeColor = document.getElementById('changeColor');
-let resetColor = document.getElementById('resetColor');
+ chrome.storage.sync.get('color', function(data) {
+     changeColor.style.backgroundColor = data.color;
+     changeColor.setAttribute('value', data.color);
+ });
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+ for (let btn of buttons) {
+     btn.addEventListener('click', (e) => {
+         console.log("***");
+         chrome.tabs.executeScript({
+             file: 'src/services/parser.js'
+         });
+     });
+ }
 
-changeColor.addEventListener('click', (e) => {
-  console.log("aaaaa");
-  document.body.style.backgroundColor = "red";
+ changeColor.addEventListener('click', (e) => {
+     console.log("aaaaa");
+     document.body.style.backgroundColor = "red";
 
-  let color = e.target.value;
+     let color = e.target.value;
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
+     //  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+     //      chrome.tabs.executeScript(
+     //          tabs[0].id, { code: 'document.body.style.backgroundColor = "' + color + '";' });
+     //  });
+ });
 
-});
-
-resetColor.addEventListener('click', (e) => {
-  console.log("ooooo");
-});
-
+ resetColor.addEventListener('click', (e) => {
+     console.log("ooooo");
+ });
