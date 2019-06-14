486b3fb2510608cb5452b3ca43cfd48cbd290934
DonKapot <alexdanin@mail.ru>
2019-06-14 23:01:05 +0300

    Reduce code. Refactor

--- src/services/parser.js
+++ src/services/parser.js
@@ -1,4 +1,5 @@
 class Element {
+
   constructor(el) {
     this.inputEl = typeof el !== "undefined" ? el : document.querySelector("div");
     this.element = this.getElement(this.inputEl);
@@ -15,7 +16,8 @@ class Element {
       return {
         'backgroundColor': style.backgroundColor,
         'color': style.color,
-        'borderColor': style.borderColor
+        'borderColor': style.borderColor,
+        'visible': style.display === 'none' || style.visibility === 'hidden' || style.visibility === 'collapse' ? false : true 
       }
     }
   
@@ -36,28 +38,34 @@ class Element {
       elemPath: elemPath,
       elemFullPath: `${parentPath} ${elemPath}`,
       elemStyle: elemStyle,
-      elemChildrens: []
+      elem: this.inputEl
     };
     return obj;
   }
 }
 
 class Dom {
+
   constructor(_domArr) {
     this.inputDom = typeof _domArr !== "undefined" ? _domArr : document.body.children;
-    this.dom = this.parse(this.inputDom);
+    this.dom = this.parseDom(this.inputDom);
   }
 
-  parse(_arr, callback) {
+  parseDom(_arr, callback) {
     let inputArr = typeof _arr !== "undefined" ? _arr : this.inputDom;
     let dom = [];
+    
     for (let i=0; i<inputArr.length; i++) {
       let element = inputArr[i];
-      let check = element.elemTag !== "SCRIPT" && element.children.length > 0;
-      if (check) {
-        dom.push(new Element(element));
+      let getElem = new Element(element);
+      let isValid = element.elemTag !== "SCRIPT" && element.children.length > 0;
+      let isCallback = typeof callback === "function";
+      let isVisible = getElem.elemStyle.visible;
+
+      if (isValid && isVisible && !isCallback) {
+        dom.push(getElem);
       }
-      if (typeof callback === "function") {
+      if (isCallback) {
         callback(mainElement);
       }
     }
@@ -65,65 +73,7 @@ class Dom {
   }
 }
 
-
-// function parseDom(_domArr, callback) {
-
-//   let inputDom = typeof _domArr !== "undefined" ? _domArr : document.body.children;
-
-//   function getStyle(el) {
-//     let style = getComputedStyle(el);
-//     return {
-//       'backgroundColor': style.backgroundColor,
-//       'color': style.color,
-//       'borderColor': style.borderColor
-//     }
-//   }
-
-//   function getPath(el) {
-//     let elemClassName = !el.className ? '' : `.${el.className.split(' ').join('.')}`;
-//     let elemId = !el.id ? '' : `#${el.id}`;
-//     return `${el.tagName}${elemClassName}${elemId}`
-//   }
-
-//   function getElement(el) {
-//     return {
-//       id: Symbol(),
-//       elemTag: el.tagName,
-//       elemParentPath: getPath(el.parentElement),
-//       elemPath: getPath(el),
-//       elemStyle: getStyle(el),
-//       elemChildrens: []
-//     };
-//   }
-
-//   function parse(arr) {
-//     let dom = [];
-//     for (let i=0; i<arr.length; i++) {
-//       let element = arr[i];
-//       let mainElement = getElement(element);
-//       let check = mainElement.elemTag !== "SCRIPT" && element.children.length > 0;
-//       if (check) {
-//         // let counter = 0;
-//         // let fullBranch = true;
-//         // while(counter < 5) {
-//         //   counter++;
-//         // }
-//         dom.push(getElement(element));
-//         // console.log(element);
-//       }
-//       if (typeof callback === "function") {
-//         callback(element);
-//       }
-//     }
-//     return dom;
-//   }
-//   return parse(inputDom);
-// }
-
-// let DOM = parseDom(document.body.children);
-
-let div = document.querySelector("div");
-let el = new Element(div);
+let el = new Element();
 let DOM = new Dom();
 console.dir(el);
 console.dir(DOM);
